807a6fe4b2564fe5cab83a92e828b7d55fb743ce
Nico du Plessis <duplessis.nico@gmail.com>
2016-01-30 01:28:31 +0200

    Starting to build authx request

--- lib/payxml.rb
+++ lib/payxml.rb
@@ -1,6 +1,7 @@
 require "payxml/version"
 require "payxml/result_codes"
 require "payxml/credit_card_types"
+require "nokogiri"
 
 module PayXML
   # Your code goes here...
@@ -13,15 +14,49 @@ module PayXML
       @paygate_password = paygate_password
     end
 
-    def process_transaction
+    def authx_no_3d_secure
     end
 
-    private
-    def message_header
-      "<protocol ver=\"#{PAYXML_VERSION_NUMBER}\" pgid=\"#{@paygate_id}\" pwd=\"#{@paygate_password}\">"
+    # budget_period
+    def authx(
+      customer_name,
+      customer_reference,
+      credit_card_number,
+      expiry_date,
+      cvv,
+      amount,
+      currency,
+      options = { budget_period: 0, bno: '' })
+
+      doc = Nokogiri::XML::Document.new
+      root = self.message_header(doc)
+
+      authx = Nokogiri::XML::Node.new "authx", doc
+      authx['cref'] = customer_reference
+      authx['cname'] = customer_name
+      authx['cc'] = credit_card_number
+      authx['exp'] = expiry_date
+      authx['budp'] = 0.to_s
+      authx['amt'] = amount
+      authx['cvv'] = currency
+      authx['bno'] = ''
+
+      root << authx
+      doc << root
+
+      puts doc
     end
 
-    def authx
+    protected
+    def message_header(xml_doc)
+      protocol_header =  Nokogiri::XML::Node.new "protocol", xml_doc
+      protocol_header['ver'] = PAYXML_VERSION_NUMBER
+      protocol_header['pgid'] = @paygate_id
+      protocol_header['pwd'] = @paygate_password
+      protocol_header
+    end
+
+    def parse_authx
     end
 
     def validate_response
