c0ded6696bf6917f2c1a01deb06349758cb6b334
Nico du Plessis <duplessis.nico@gmail.com>
2016-02-01 22:04:07 +0200

    More authtx parsing added

--- lib/payxml/authtx.rb
+++ lib/payxml/authtx.rb
@@ -5,11 +5,12 @@ module PayXML
       attr_accessor :customer_name
       attr_accessor :customer_reference
       attr_accessor :budget_period
-      attr_accessor :expiry_date
       attr_accessor :amount
       attr_accessor :currency
       attr_accessor :bno
       attr_accessor :credit_card_number
+      attr_accessor :cvv
+      attr_accessor :expiry_date
 
       def parse(xml_string)
         super(xml_string)
@@ -18,6 +19,9 @@ module PayXML
         authtx = doc.xpath("//authtx").first
         self.customer_reference = authtx['cref']
         self.customer_name = authtx['cname']
+        self.credit_card_number = authtx['cc']
+        self.expiry_date = authtx['exp']
+        self.cvv = authtx['cvv']
       end
     end
 
